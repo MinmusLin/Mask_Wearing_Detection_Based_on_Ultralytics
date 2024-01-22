1cdb811cbb7934e8e243600fe80a5dde8f11405f
MinmusLin <minmuslin@outlook.com>
2024-01-22 21:53:18 +0800

    2024/1/22 Updated

--- code/train.py
+++ code/train.py
@@ -2,7 +2,5 @@
 
 from ultralytics import YOLO
 
-detaset_name = "facemasks" # Dataset name
-
 model = YOLO("yolov8n.pt")
-model.train(data = "datasets/" + detaset_name + "/data.yaml", workers = 0, epochs = 50, batch = 24)
+model.train(data="datasets/facemasks/data.yaml", workers=0, epochs=100, batch=16)
